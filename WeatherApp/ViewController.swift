0fa880b98cf689f0192c090dc91e73f234206d0f
Vincent Cubit <vinniecubit@yahoo.com>
2022-03-06 15:46:42 -0600

    Init

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -7,11 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+
+    @IBOutlet weak var currentTemp: UILabel!
+    @IBOutlet weak var currentConditions: UILabel!
+    @IBOutlet weak var dewPoint: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+        if let url = NSURL(string: "https://api.darksky.net/forecast/ae58c5fa7285b492f6a553d200018d9e/42.5917,88.4334") {
+            if let data = NSData(contentsOf: url as URL) {
+                do {
+                    let parsed = try JSONSerialization.jsonObject(with: data as Data, options: JSONSerialization.ReadingOptions.fragmentsAllowed) as! [ String: AnyObject ]
+                    
+                    
+                    let newDict = parsed
+                    
+                    
+                    self.currentTemp.text = "\(String(describing: newDict["currently"]!["temperature"]!!))"
+                    self.currentConditions.text = "\(String(describing: newDict["currently"]!["summary"]!!))"
+                    self.dewPoint.text = "\(String(describing: newDict["currently"]!["dewPoint"]!!))"
+                    
+                    
+                } catch _ as NSError {
+                    print()
+                }
+            }
+        }
     }
 
 
